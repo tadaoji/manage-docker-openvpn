8374d5ab93e5b759984642b3f9dbbce4b9627657
tadaoji <ojisan@ofuton.org>
2021-04-18 18:58:22 +0900

    Changed disconnect_account.sh

--- disconnect_account.sh
+++ disconnect_account.sh
@@ -1,56 +1,44 @@
 #! /bin/sh
-function color_echo() {
-  local ESC
-  ESC=$(printf '\033')
-  local color
 
-  if [ -z $2 ]; then
-    echo $1
-    exit 1
-  fi
+source ./lib.sh
+value_from_env
+get_docker_info
 
-  if [ $2 = black ]; then
-    color=30m
-  elif [ $2 = red ]; then
-    color=31m
-  elif [ $2 = green ]; then
-    color=32m
-  elif [ $2 = yellow ]; then
-    color=33m
-  elif [ $2 = blue ]; then
-    color=34m
-  elif [ $2 = magenta ]; then
-    color=35m
-  elif [ $2 = cyan ]; then
-    color=36m
-  elif [ $2 = white ]; then
-    color=37m
-  else
-    color=0
-  fi
-
-  if [ $color = 0 ]; then
-    echo $1
-  else
-    echo "${ESC}[${color}${1}${ESC}[m"
-  fi
-}
 # telnetでコンテナ内のOpenVPN Managerに接続してrevoke対象を即時切断する
-echo -e -n "\n"
-cd ./docker_ovpn
-# container名抽出
-DOCKER_CONTAINER_NAME=`docker-compose ps | grep -v Name | grep -v '\-\-\-' | awk '{print $1}'`
-# containerのIPアドレスを抽出する
-DOCKER_CONTAINER_IP=`docker container inspect ${DOCKER_CONTAINER_NAME} | grep IPAddress | grep -Eo '([1-2]?[0-9]{0,2}\.){3,3}[1-2]?[0-9]{0,2}'`
+# telnetでの操作
+color_echo "---------- Start check ovpn connecting status ----------" magenta
+MANAGER_RESPONSE=`(sleep 3; echo "status"; sleep 1; echo "quit"; sleep 1) | telnet ${DOCKER_CONTAINER_IP} 7505`
+color_echo "`echo "${MANAGER_RESPONSE}" | grep -E ^${1},`" cyan
+echo "${MANAGER_RESPONSE}" | grep -E ^${1}, 2>&1 1> /dev/null
 if [ $? -eq 0 ]; then
-  color_echo "Succeeded in getting Docker_container IP: ${DOCKER_CONTAINER_IP} NAME: ${DOCKER_CONTAINER_NAME}" yellow
-    else
-  color_echo "Failed in getting Docker_container IP & NAME"
+  color_echo "Find connecting USER: ${1}" yellow
+  echo -e "\n"
+  color_echo "---------- kill connection from USER: ${1} ----------" magenta
+  (sleep 3; echo "kill ${1}" sleep3; echo quit; sleep 1) | telnet ${DOCKER_CONTAINER_IP} 7505
+  color_echo "Done" yellow
+else
+  color_echo "Can't find connection from USER: ${1} now. (no problem)" yellow
 fi
 
-MANAGER_REV=`(sleep 3; echo "status"; sleep 1; echo "quit"; sleep 1) | telnet ${DOCKER_CONTAINER_IP} 7505`
-HIGE=`echo "${MANAGER_REV}" | grep -E ^${1},`
-color_echo "$`echo "${MANAGER_REV}" | grep -E ^${1},`" cyan
 
+
+
+# telnetでコンテナ内のOpenVPN Managerに接続してrevoke対象を即時切断する
+#echo -e -n "\n"
+#cd ./docker_ovpn
+## container名抽出
+#DOCKER_CONTAINER_NAME=`docker-compose ps | grep -v Name | grep -v '\-\-\-' | awk '{print $1}'`
+## containerのIPアドレスを抽出する
+#DOCKER_CONTAINER_IP=`docker container inspect ${DOCKER_CONTAINER_NAME} | grep IPAddress | grep -Eo '([1-2]?[0-9]{0,2}\.){3,3}[1-2]?[0-9]{0,2}'`
+#if [ $? -eq 0 ]; then
+#  color_echo "Succeeded in getting Docker_container IP: ${DOCKER_CONTAINER_IP} NAME: ${DOCKER_CONTAINER_NAME}" yellow
+#    else
+#  color_echo "Failed in getting Docker_container IP & NAME"
+#fi
+#
+#MANAGER_REV=`(sleep 3; echo "status"; sleep 1; echo "quit"; sleep 1) | telnet ${DOCKER_CONTAINER_IP} 7505`
+#HIGE=`echo "${MANAGER_REV}" | grep -E ^${1},`
+#color_echo "$`echo "${MANAGER_REV}" | grep -E ^${1},`" cyan
+#
 # telnetでの操作
 #        (sleep 3; echo "kill ${1}" sleep3; echo quit; sleep 1) | telnet ${DOCKER_CONTAINER_IP} 7505
